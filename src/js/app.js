49ad9e4b427a843c4a849ff11dd168eb84d1ae4a
Andrii <chuhcu.andreimax@gmail.com>
2025-02-13 21:04:35 +0200

    Adding dark theme and shortinmg js code

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,30 @@
 const btns = document.querySelectorAll('.card__quastion-btn')
+const changeThemeBtn = document.querySelector('#changeThemeBtn')
+
 
 btns.forEach(btn => {
 	btn.addEventListener('click', () => {
 		const quastion = btn.closest('.card__quastion')
 		const desc = quastion.querySelector('.card__desc')
 
-		if (btn.classList.contains('unchecked')) {
-			btn.classList.remove('unchecked')
-			btn.classList.add('checked')
-			desc.style.display = 'none'
-			quastion.style.paddingBottom = '0'
-
-
-		} else {
-			btn.classList.remove('checked')
-			btn.classList.add('unchecked')
+		btn.classList.toggle('checked')
+		if (btn.classList.contains('checked')) {
 			desc.style.display = 'inline'
 			quastion.style.paddingBottom = '1rem'
+		} else {
+			desc.style.display = 'none'
+			quastion.style.paddingBottom = '0'
 		}
 		
 	})
+})
+
+
+window.addEventListener('load', () => {
+	document.documentElement.setAttribute('data-theme', 'light')
+})
+
+changeThemeBtn.addEventListener('click', () => {
+	document.documentElement.setAttribute('data-theme', 
+		document.documentElement.getAttribute('data-theme') == 'light' ? 'dark' : 'light')
 })
